2c99bb8ecd76025bb6447de16a0377a689a88b53
Ícar N. S <icar.nin@protonmail.com>
2023-06-24 23:11:52 +0200

    fix root redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
-import React from "react";
-import { redirect } from "next/navigation";
+import React, { useEffect } from "react";
+import { Wrapper } from "@dcm/components/wrapper";
+import { useRouter } from "next/router";
 
-const Home: React.FC = () => <></>;
+const Home: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    window.location.href = "/paraules";
+  }, []);
+  return <Wrapper></Wrapper>;
+};
 
 export default Home;
