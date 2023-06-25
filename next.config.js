82590f7c02d507f848e5aacc290fc83a788abeba
Ícar N. S <icar.nin@protonmail.com>
2023-06-25 20:20:12 +0200

    bsize--

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
+  swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
