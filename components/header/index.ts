5fbfbb9ec35f6b47240f374bc8ebbf967e36ad16
Ícar N. S. <icar.nin@protonmail.com>
2023-12-28 13:15:34 +0100

    static header

--- components/header/index.ts
+++ components/header/index.ts
@@ -1,5 +1,3 @@
 import Header from "./component";
-import Skeleton from "./skeleton";
 
 export default Header;
-export { Skeleton };
--- components/header/skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Skeleton: React.FC = () => (
-  <div className="h-32 w-full mb-6 bg-bookBlue dark:bg-black animate-pulse"></div>
-);
-
-export default Skeleton;
