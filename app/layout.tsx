77e4240d645d18bb2ca1bb5052872547358b1df1
Ícar Nin Solana <icar.nin@protonmail.com>
2024-01-27 18:10:37 +0100

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 
 const raleway = Raleway({ subsets: ["latin"] });
@@ -30,6 +31,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       >
         <main className="px-4 mb-12 max-w-4xl mx-auto mt-36">{children}</main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
