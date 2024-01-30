2aba231d845ba0d448606a095c081542cb0dcb53
Ícar N. S. <icar.nin@protonmail.com>
2024-01-30 23:09:44 +0100

    remove preact

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
