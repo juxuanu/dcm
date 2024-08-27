8f869a7ef8d3b2f7219c69b51c4884b17b1051a3
Ícar Nin Solana <icar.nin@protonmail.com>
2024-08-27 18:04:47 +0200

    improve metadata (for SEO)

--- app/_site-config.ts
+++ app/_site-config.ts
@@ -1,1 +1,5 @@
 export const url = "https://diccionari.icarns.xyz";
+export const title = "Mallorquí per catalans";
+export const description =
+  "Mallorquí per catalans: expressions, paraules i curiositats.";
+export const keywords = ["mallorquí", "català", "expressions", "curiositats"];
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,30 @@ import { Raleway } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
+import { description, keywords, title, url } from "@dcm/app/_site-config";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mallorquí per catalans",
-  keywords: ["mallorquí", "català", "expressions", "curiositats"],
-  description: "Mallorquí per catalans: expressions, paraules i curiositats.",
+  title,
+  keywords,
+  description,
+  openGraph: {
+    siteName: title,
+    countryName: "Països Catalans",
+    title,
+    authors: ["Aina Delgado", "Ícar N. S."],
+    url,
+    description,
+    locale: "ca",
+    tags: keywords,
+  },
   authors: [
     {
       name: "Aina Delgado",
       url: new URL("https://www.instagram.com/ainadelsol/"),
     },
-    { name: "Ícar Nin", url: new URL("https://icarns.xyz") },
+    { name: "Ícar N. S.", url: new URL("https://icarns.xyz") },
   ],
 };
 
