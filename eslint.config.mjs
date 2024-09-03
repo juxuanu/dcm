49a0304dcb033863ad0b2621947e96a173eeee76
Ícar Nin Solana <icar.nin@protonmail.com>
2024-09-03 18:56:27 +0200

    upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    ignores: ["**/.github", "**/node_modules", "**/out", "**/next-env.d.ts"],
+  },
+  ...compat.extends("next/core-web-vitals", "eslint:recommended"),
+];
