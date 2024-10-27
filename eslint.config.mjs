2231099b1cb5d0fd7b5d93d874fb9756bb69fd2f
Ícar N. S. <icar.nin@protonmail.com>
2024-10-27 12:00:01 +0100

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,16 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+const config = [
+  {
     ignores: ["**/.github", "**/node_modules", "**/out", "**/next-env.d.ts"],
-}, ...compat.extends("next/core-web-vitals", "eslint:recommended")];
+  },
+  ...compat.extends("next/core-web-vitals", "eslint:recommended"),
+];
+
+export default config;
