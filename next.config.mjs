620befa381ad320f9fab6fd9e6119fcee3ab7f60
Ícar N. S. <icar.nin@protonmail.com>
2025-01-27 19:42:27 +0100

    react compiler

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import withExportImages from "next-export-optimize-images";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  experimental: {
+    reactCompiler: true
+  }
 };
 
 export default withExportImages(nextConfig);
