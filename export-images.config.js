c41a22dec5b7f487793c45b45c3dfcf28bb53e4e
Ícar Nin Solana <icar.nin@protonmail.com>
2025-07-06 10:13:18 +0200

    remove optimize images

--- export-images.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import("next-export-optimize-images").Config}
- */
-const config = {
-  generateFormats: ["avif", "webp"],
-  cacheDir: ".next/cache/optimized-images",
-};
-
-module.exports = config;
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-import withExportImages from "next-export-optimize-images";
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "export",
 };
 
-export default withExportImages(nextConfig);
+export default nextConfig;
